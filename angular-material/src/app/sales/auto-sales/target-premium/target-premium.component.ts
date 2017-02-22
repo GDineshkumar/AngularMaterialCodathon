f30edd569729c725fc5f6fd6fa106ef162a8afce
CO22813 <CO22813@5CG4480G6R.ad1.prod>
2017-02-22 14:05:11 -0500

    commit the service

---
+++ angular-material/src/app/sales/auto-sales/target-premium/target-premium.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-target-premium',
+  templateUrl: 'target-premium.component.html',
+  styleUrls: ['target-premium.component.css']
+})
+export class TargetPremiumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  targetPremiumDispValue = 100;
+ // @Input() value;
+ //
+ //  valueChanged(){
+ //
+ //
+ //
+ //  }
+
+}
