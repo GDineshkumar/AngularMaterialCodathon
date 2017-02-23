01dcb4db88a7a061be1112456b3f36aa0a5c121f
js87327 <jeffreysapia@gmail.com>
2017-02-23 14:54:49 -0500

    Commit sliders working

--- src/app/sales/auto-sales/quote-summary/quote-summary.component.ts
+++ src/app/sales/auto-sales/quote-summary/quote-summary.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Inject} from '@angular/core';
+import {TargetPremium} from "../target-premium/target-premium";
 
 @Component({
   selector: 'sales-auto-quote-summary',
   templateUrl: 'quote-summary.component.html',
   styleUrls: ['quote-summary.component.css']
 })
-export class QuoteSummaryComponent implements OnInit {
+export class QuoteSummaryComponent  {
 
-  constructor() { }
+  targetPremium: TargetPremium;
+  percentChange: number;
 
-  ngOnInit() {
+  constructor(@Inject(TargetPremium) targetPremium) {
+    this.targetPremium = targetPremium;
+  }
+
+  numberWithCommas(x) {
+    x = Math.round(x);
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  toPercentage(max: number, value: number) {
+    return Math.round(max * value);
   }
 
 }
