4983df05779d2bbd3386885c1e761283332a6335
Jeff Sapia <jeffreysapia@gmail.com>
2017-02-22 19:16:18 -0500

    Commit event binding functional

--- src/app/sales/auto-sales/personal-info/personal-info-history/personal-info-history.component.ts
+++ src/app/sales/auto-sales/personal-info/personal-info-history/personal-info-history.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalInfoHistoryComponent implements OnInit {
 
-  personalInfoHistory:PersonalInfoHistory;
-
   ngOnInit() {
   }
 
